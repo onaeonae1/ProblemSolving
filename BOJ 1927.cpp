93cc224a3c8f9c4b40040e3f208cf317a65c83f5
Jongwon Baek <41908963+onaeonae1@users.noreply.github.com>
2020-01-24 16:30:06 +0900

    Priority_Queue
    STL의 우선순위 큐는 기본적으로 최대힙으로 선언되므로
    최소힙처럼 사용하고 싶다면 가중치에 음수를 부여하여 사용하면 된다.

---
+++ BOJ 1927.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<queue>
+using namespace std;
+int main() {
+	priority_queue<int, vector<int>> pq; //비교 연산자 생략하여 선언 : 최대 힙으로 설정됨
+	int c;
+	scanf("%d", &c);
+	while (c--) {
+		int num;
+		scanf("%d", &num);
+		if (num == 0) { //출력 명령
+			if (pq.empty()) {
+				printf("0\n");
+			}
+			else {
+				printf("%d\n", pq.top() * -1);
+				pq.pop();
+			}
+		}
+		else {
+			int temp = num * -1;
+			pq.push(temp);
+			//음수로 사용하는 이유
+			//STL에서의 우선순위 큐는 최대힙이므로
+			//음수로 값 넣어주면 최소힙처럼 쓸 수 있음
+		}
+	}
+	return 0;
+}
