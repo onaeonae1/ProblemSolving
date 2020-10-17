05c1c61381f3534fbcccf6754c203fc29557febe
Jongwon Baek <onaeonae2@gmail.com>
2020-10-17 11:57:14 +0900

    Update BOJ 2252.cpp

--- BOJ 2252.cpp
+++ BOJ 2252.cpp
@@ -1,30 +1,33 @@
-#include<stdio.h>
-#include <queue>
-#define L 32001
+#include<iostream>
+#include<queue>
+#include<vector>
+#define L 32005
 using namespace std;
-
-int indegree[L];
-vector<int> vertex[L];
-
+int indegree[L]; 
+vector<int> edge[L]; 
 int main() {
-	int n, m, a, b;
+	int n, m, start, end;
 	scanf("%d %d", &n, &m);
 	for (int i = 0; i < m; i++) {
-		scanf("%d %d", &a, &b);
-		indegree[b]++; //진입 차수. b에 하나 추가로 들어온다.
-		vertex[a].push_back(b); //a는 b를 가리키고 있다.
+		scanf("%d %d", &start, &end);
+		indegree[end]++; //end node로의 진입차수
+		edge[start].push_back(end); //start->end 간선 추가
 	}
-	queue<int> now;
-	for (int i = 1; i <= n; i++)
-		if (indegree[i] == 0) now.push(i);
-
-	while (!now.empty()) {
-		int i = now.front(); now.pop();
-		printf("%d ", i);
-
-		for (int j = 0; j < vertex[i].size(); j++)
-			if (--indegree[vertex[i][j]] == 0)
-				now.push(vertex[i][j]);
+	queue<int> now; //위상 정렬에 사용할 큐
+	for (int i = 1; i <= n; i++) { //노드들 중에서
+		if (indegree[i] == 0) {
+			now.push(i);
+		}
+	}
+	while (!now.empty()) { 
+		int t = now.front();
+		now.pop();
+		printf("%d ", t); //해당 노드 방문
+		for (int j = 0; j < edge[t].size(); j++) {
+			if (--indegree[edge[t][j]] == 0) { //간선제거후 0이면
+				now.push(edge[t][j]);
+			}
+		}
 	}
 	return 0;
 }
