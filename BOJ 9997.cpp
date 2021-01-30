c3bed052a357bf7e268ac290c74432f490c9a1d1
Jongwon Baek <onaeonae2@gmail.com>
2021-01-30 23:01:39 +0900

    Bitmask, BFS
    문제 해결!

--- BOJ 9997.cpp
+++ BOJ 9997.cpp
@@ -1,41 +1,33 @@
-// 0000.. 01 : 아무것도 없음
-// 0000.. 11 : a 만 있음
 #include<iostream>
 #include<string>
 #include<vector>
 using namespace std;
-vector<int> wordsVector;
-int answer;
-int c;
-int fullCombination = (1 << 26) - 1;
-void solve(int index, int state) {
-	//index : 확인중인 word
-	//state : 현재 bitmask의 상태
-	if (index == c-1) {
-		if (state == fullCombination) {
-			answer++;
+typedef long long ll;
+int n;
+vector<ll> words;
+int solve(int index, int state) {
+	if (index == n) {
+		if (state == (1 << 26) - 1) {
+			return 1;
 		}
-		return;
+		else {
+			return 0;
+		}
 	}
-	else {
-		solve(index + 1, state | wordsVector[index + 1]); //다음 word를 포함
-		solve(index + 1, state); //다음 word를 포함시키지 않음
-	}
+	//선택, 선택안함으로 확장
+	return solve(index + 1, state | words[index]) + solve(index + 1, state);
 }
 int main() {
-	ios_base::sync_with_stdio(false); 
-	cin.tie(NULL);
-	scanf("%d", &c);
-	for(int i=0; i<c; i++) {
-		string word;
-		cin >> word;
-		int temp = 0;
-		for (int j = 0; j < word.size(); j++) {
-			temp = temp | (1 << word.at(j) - 'a');
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++) {
+		string str;
+		cin >> str;
+		ll temp = 0;
+		for (auto alpha : str) { //rnage-based auto
+			temp |= 1 << (alpha - 'a');
 		}
-		wordsVector.push_back(temp);
+		words.push_back(temp);
 	}
-	solve(-1, 0);
-	printf("%d\n", answer);
+	printf("%d", solve(0, 0));
 	return 0;
 }
